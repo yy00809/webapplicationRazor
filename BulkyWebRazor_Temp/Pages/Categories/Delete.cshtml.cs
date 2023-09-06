3563d5569076d9bdcced59f3c8bc3934bea0a95b
yy00809 <58527321+yy00809@users.noreply.github.com>
2023-09-06 15:31:34 +0100

    simple web in razor

---
+++ BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+	public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+                Category = _db.Categories.Find(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? categoryFromDb = _db.Categories.Find(Category.Id);
+            if (categoryFromDb == null)
+                return NotFound();
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
+            return RedirectToPage("Index");
+        }
+    }
+}
